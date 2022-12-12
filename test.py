10e0bf15fb625eba2236c336c5ca0a01ab216d9e
Linuzzz <pabrizzo@gmail.com>
2022-12-12 12:03:49 +0100

    Create test.py

---
+++ test.py
@@ -0,0 +1,10 @@
+#!/bin/python3
+
+print('i cagnolini sono carini per te?')
+
+risposta=input()
+
+if str(risposta)=='si':
+        print('hai un cane di nome puffolo')
+else:
+        print('love cagnolini sei brutto')
